40fcde30e64ba7da964816910e6ed97eea7be753
leaalbano <89825014+leaalbano@users.noreply.github.com>
2021-11-28 15:41:34 -0800

    Delete checkbox.js

--- checkbox.js
+++
@@ -1,8 +0,0 @@
-/* Checks if box is clicked & adds a check inside */
-var ifCheckView;
-if (test_checkBox.checked) {
-    ifCheckView = "Checked";
-}
-else if (!test_checkBox.checked) {
-    ifCheckView = "Not Checked";
-}
