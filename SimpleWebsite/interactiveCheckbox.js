2965bfea173c0c3efa25acb309decb1b38081570
leaalbano <89825014+leaalbano@users.noreply.github.com>
2021-11-28 15:42:08 -0800

    Create interactiveCheckbox.js

---
+++ SimpleWebsite/interactiveCheckbox.js
@@ -0,0 +1,8 @@
+/* Checks if box is clicked & adds a check inside */
+var ifCheckView;
+if (test_checkBox.checked) {
+    ifCheckView = "Checked";
+}
+else if (!test_checkBox.checked) {
+    ifCheckView = "Not Checked";
+}
